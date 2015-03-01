dbaa594a0756664f28d71bda91b264cfef9fd3d2
Ken McLennan <th3sku11@gmail.com>
2015-03-01 17:16:54 +0000

    added gemfile, rack config

--- server.rb
+++ server.rb
@@ -1,26 +1,32 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'json'
 
-ROLLER = Random.new
 
-post '/roll' do
-  sides    = Integer(params.fetch(:d){ 20 })
-  quantity = Integer(params.fetch(:q){ 1 })
-  roll     = quantity.times.inject(0) { |total,_| total + ROLLER.rand(1...sides) }
+module HipChatDiceBag
+  class Server < Sinatra::Base
 
-  name = if request.content_type=="application/json"
-    body = JSON.parse(request.body.read)
-    body['item']['message']['from']['name']
-  else
-    'you'
+    ROLLER = Random.new
+
+    post '/roll' do
+      sides    = Integer(params.fetch(:d){ 20 })
+      quantity = Integer(params.fetch(:q){ 1 })
+      roll     = quantity.times.inject(0) { |total,_| total + ROLLER.rand(1...sides) }
+
+      name = if request.content_type=="application/json"
+        body = JSON.parse(request.body.read)
+        body['item']['message']['from']['name']
+      else
+        'you'
+      end
+      
+      status 200
+      
+      body JSON.generate({
+        color: "green",
+        message: "#{name} rolled a #{roll}!",
+        notify: false,
+        message_format: "text"
+      })
+    end
   end
-  
-  status 200
-  
-  body JSON.generate({
-    color: "green",
-    message: "#{name} rolled a #{roll}!",
-    notify: false,
-    message_format: "text"
-  })
 end
